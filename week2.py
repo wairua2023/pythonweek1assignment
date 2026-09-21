858b00af278ccf67246e1f7ea1f90014e5c3b7f1
wairua <134479497+wairua2023@users.noreply.github.com>
2026-09-21 09:29:13 +0300

    Delete week2.py

--- week2.py
+++
@@ -1,6 +0,0 @@
-# simple bill calculator
-price = float(input("Enter price of one item: "))
-quantity = int(input("Enter the quantity: "))
-total = price * quantity
-
-print(f"{quantity} items at {price:.2f} total {total:.2f}")
